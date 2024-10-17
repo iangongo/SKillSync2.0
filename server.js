c3fd972368d4a7a8902edf4abe8831b37e903ef5
iangongo <ianmwanyaegongo@gmail.com>
2024-10-16 18:27:13 -0700

    more bug fixes

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Serve the index.html directly from the root folder
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
